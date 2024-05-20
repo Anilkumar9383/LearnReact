77aa3db78720169f2df819fa67ae6a5eff5c90d1
Anil kumar <anilchaudhary7489@gmail.com>
2024-05-21 00:16:23 +0530

    url

--- src/components/Common/ApiUrl.jsx
+++ src/components/Common/ApiUrl.jsx
@@ -1,4 +1,4 @@
-const apiURL = "https://localhost:7061/api/";
-//const apiURL = "https://akwebapi.somee.com/api/";
+//const apiURL = "https://localhost:7061/api/";
+const apiURL = "https://akwebapi.somee.com/api/";
 
 export default apiURL;
